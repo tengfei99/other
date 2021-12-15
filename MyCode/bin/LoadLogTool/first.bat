c39c96ff86e8b548a9d363d8fc669c9dc099efd2
tengfei99 <39527074+tengfei99@users.noreply.github.com>
2021-12-15 12:47:01 +0800

    Delete MyCode directory

--- MyCode/bin/LoadLogTool/first.bat
+++
@@ -1,4 +0,0 @@
-set CLASSPATH=%CLASSPATH%;jsch-0.1.41.jar
-javac DataModel.java
-javac ZipTool.java
-javac LoadLogTool.java
--- MyCode/src/LoadLogTool/LoadLogTool.bat
+++
@@ -1,5 +0,0 @@
-cd D:\lishicun\logs
-del *.log
-cd D:\lishicun\UKBERSEE1
-set CLASSPATH=.;%CLASSPATH%;jsch-0.1.41.jar
-java LoadLogTool
